0a8c5f2cfae0c83ed16e748761a4c746d140cfb9
layerfi <layerfi@protonmail.com>
2021-01-04 03:20:34 +0900

    change: LF Token Contract address

--- src/lib/contracts.js
+++ src/lib/contracts.js
@@ -2,7 +2,7 @@ import ERC20 from "./abi/ERC20.json";
 import UNIPOOL from "./abi/UNIPool.json";
 import UNITOKEN from "./abi/UNIToken.json";
 
-export const LF_ADDRESS = "0xdac17f958d2ee523a2206206994597c13d831ec7";
+export const LF_ADDRESS = "0x503d58fe62578413c837fe92c1616e68d5f135d3";
 
 export const ERC20_ABI = ERC20;
 export const UNIPOOL_ABI = UNIPOOL;
